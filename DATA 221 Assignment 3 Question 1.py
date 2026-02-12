f0e350e32178cea03aede272acc9ebfca3fa6007
SitaraTrue <sitaratrue@gmail.com>
2026-02-12 14:43:24 -0700

    Answer questions for Q1

--- DATA 221 Assignment 3 Question 1.py
+++ DATA 221 Assignment 3 Question 1.py
@@ -21,3 +21,17 @@ print(f"Median: {crime_df["ViolentCrimesPerPop"].median()}")
 print(f"Standard deviation: {crime_df["ViolentCrimesPerPop"].std()}")
 print(f"Minimum: {crime_df["ViolentCrimesPerPop"].min()}")
 print(f"Maximum: {crime_df["ViolentCrimesPerPop"].max()}")
+
+'''
+The mean is 0.44119, and the median is 0.39.
+Since the mean is greater than the median, the distribution is right-skewed.
+The upper 50% of the values are pulling the mean towards the right.
+'''
+
+'''
+Extreme values affect the mean more than the median.
+Mean is affected by the value of each data point. Median is affected by the order of values.
+Consider: 1, 100, 101, 102, 103 and 99, 100, 101, 102, 103
+The mean is heavily affected by the change from 1 to 99 (81.4 vs 101).
+The median is exact same for both (101) because the middle value is the exact same.
+'''
