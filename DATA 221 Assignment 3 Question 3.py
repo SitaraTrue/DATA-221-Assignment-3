8f9f6e07d2c4aa15e1eaaad169dddcfc62be06cc
SitaraTrue <sitaratrue@gmail.com>
2026-02-12 15:47:32 -0700

    Create feature matrix and train-test split for Q3

---
+++ DATA 221 Assignment 3 Question 3.py
@@ -0,0 +1,23 @@
+''' Question 3
+Load kidney_disease.csv into a pandas DataFrame.
+Create a feature matrix X that contains all columns except classification.
+Create a label vector y using the classification column.
+Then, split the dataset into training (70%) and testing (30%) data.
+Use train_test_split with a fixed random_state.
+
+After performing the split, write comments in your code explaining:
+- Why we should not train and test a model on the same data
+- What the purpose of the testing set is
+'''
+
+# Reads csv into a dataframe
+from sklearn.model_selection import train_test_split
+import pandas as pd
+kidney_disease_df = pd.read_csv("kidney_disease.csv")
+
+# Creates feature matrix X of all columns except "classification" and saves "classification" as label vector Y
+x = kidney_disease_df.drop("classification", axis=1)
+y = kidney_disease_df["classification"]
+
+# Splits training and testing data with a fixed seed
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=19)
