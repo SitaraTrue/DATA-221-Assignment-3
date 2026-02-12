b4909021369f5d53b9c2da22552a7fc5bd7b877b
SitaraTrue <sitaratrue@gmail.com>
2026-02-12 14:47:06 -0700

    Add Q2 file

---
+++ DATA 221 Assignment 3 Question 2.py
@@ -0,0 +1,14 @@
+''' Question 2
+Using crime.csv and the ViolentCrimesPerPop column, create two plots using matplotlib.
+First, create a histogram that shows how the values are distributed.
+Second, create a box plot for the same data.
+Each plot must include a clear title and axis labels.
+
+After generating the plots, write comments in your code describing:
+- What the histogram shows about how the data values are spread
+- What the box plot shows about the median
+- Whether the box plot suggests the presence of outliers
+'''
+
+import pandas
+import matplotlib
